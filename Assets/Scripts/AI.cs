8e4db739f9e917dff21b322ec0adbd08324e4085
Crazyafk <61631722+Crazyafk@users.noreply.github.com>
2023-07-07 21:07:39 +0100

    Basic game loop
    (non-functional)

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AI : MonoBehaviour
 {
+    enum Status{Playing, Stuck, Bust}
+    Status status;
+
+    int points; //how many rounds this AI has won
+
     CardManager cardManager;
     List<Card> deck;
     int value;
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    CardManager cardManager;
+    Queue<Card> toBeDealt;
+
+    AI aiOne, aiTwo, aiThree;
+    int whichAiIsPlayer; //0-2 //Which AI is do we want to win?
+    int whichAiDueTurn; //0-2 //Which AI is due to take their turn next?
+    
+    void Start()
+    {
+        cardManager = GetComponent<CardManager>();
+
+        toBeDealt = new Queue<Card>();
+        for(int i = 0; i < 3; i++)
+        {
+            toBeDealt.Enqueue(cardManager.ChooseCard());
+        }
+    }
+
+    public void NextTurn()
+    {
+        if(whichAiDueTurn == 0){aiOne.TakeTurn();}
+        if(whichAiDueTurn == 1){aiOne.TakeTurn();}
+        if(whichAiDueTurn == 2){aiOne.TakeTurn();}
+
+        whichAiDueTurn = (whichAiDueTurn + 1) % 3;
+    }
+}
